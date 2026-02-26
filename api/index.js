f57aa61ec647200dd350ec743e81acf6f9b7bc14
Shunfengbali <3480732102@qq.com>
2026-02-26 09:26:49 +0800

    更新 index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
+//CORS配置
 const express = require('express');
+const cors = require("cors");
 const app = express();
+app.use(cors({origin:"https://shunfengbali.github.io/Website A01"}));
+
+//连接
+
 app.get('/', (req, res) => res.send('Express跑起来啦！'));
 module.exports = app;
